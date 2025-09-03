7f942d0509b9b9efe5c43f729f5c323a1862577b
kritzyy15 <dev.gayathri.15@gmail.com>
2025-09-03 17:09:40 +0000

    add isprime

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# GcdSolution, ArmstrongSolution, LCMSolution
-from math_utils import Divisors
+# GcdSolution, ArmstrongSolution, LCMSolution, Divisors
+from math_utils import Prime
 
 if __name__ == "__main__":
     # gcd = GcdSolution()
     # arm = ArmstrongSolution()
     # lcm = LCMSolution()
-    div = Divisors()
+    # div = Divisors()
+    prime = Prime()
 
     # print(gcd.GCD(48, 18))
     # print(arm.isArmstrong(123))
     # print(lcm.LCM(48, 18))
-    print(div.divisors(8))
+    # print(div.divisors(8))
+    print(prime.isPrime(11))
--- math_utils/__init__.py
+++ math_utils/__init__.py
@@ -2,5 +2,6 @@ from .gcd import Solution as GcdSolution
 from .armstrong_num import Solution as ArmstrongSolution
 from .lcm import Solution as LCMSolution
 from .divisor import Solution as Divisors
+from .prime import Solution as Prime
 
-__all__ = ["GcdSolution", "ArmstrongSolution", "LCMSolution", "Divisors"]
+__all__ = ["GcdSolution", "ArmstrongSolution", "LCMSolution", "Divisors", "Prime"]
